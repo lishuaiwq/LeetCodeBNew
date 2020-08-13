2a21be9b7d7bbd7c52dd297da87a756e926adddf
unknown <liilshuai@163.com>
2020-08-14 00:55:57 +0800

    8_14_0_55

---
+++ java/src/main/solution/Solution16.java
@@ -0,0 +1,33 @@
+package main.solution;
+
+import java.util.ArrayList;
+
+/**
+ * 从上往下打印出二叉树的每个节点，
+ * 同层节点从左至右打印。
+ * 层序遍历二叉树
+ *
+ *
+ * 借助队列：将当前节点左子树放入队列，在将其右子树放入队列。然后拿出队列得节点继续以此类推。
+ *
+ */
+public class Solution16 {
+    public ArrayList<Integer> PrintFromTopToBottom(Solution13.TreeNode root) {
+           return null;
+    }
+
+
+    /**
+     public class TreeNode {
+     int val = 0;
+     TreeNode left = null;
+     TreeNode right = null;
+
+     public TreeNode(int val) {
+     this.val = val;
+
+     }
+
+     }
+     */
+}
